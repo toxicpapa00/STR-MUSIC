f1619e2896f5d59d0bf0ad94f0d57e27ce1c3ea3
Gyanesh Patel <gyaneshpatel605@gmail.com>
2025-12-16 14:03:38 +0530

    Update Youtube.py

--- SHUKLAMUSIC/platforms/Youtube.py
+++ SHUKLAMUSIC/platforms/Youtube.py
@@ -13,7 +13,7 @@ from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
 from requests.adapters import HTTPAdapter
 from urllib3.util.retry import Retry
-from py_yt import VideosSearch
+from youtubesearchpython import VideosSearch
 import base64
 from SHUKLAMUSIC import LOGGER
 from SHUKLAMUSIC.utils.database import is_on_off
