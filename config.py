8a7ed3adab390daa88a574d857cdbf8a219d58f3
Gyanesh Patel <gyaneshpatel605@gmail.com>
2025-12-16 13:52:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -45,7 +45,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 DEEP_API = getenv("DEEP_API")
 
 # Git
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/TEAM-ISHA/STR-MUSIC")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/TOXICPAPA00/STR-MUSIC")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
